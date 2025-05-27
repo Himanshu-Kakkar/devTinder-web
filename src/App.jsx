bc788dbdb80936082943ed00b4bd20f6e0fcfb9d
Himanshu Kakkar <himanshukakkar917@gmail.com>
2025-05-27 17:09:30 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,31 @@
-import React  from "react";
+
+import NavBar from "./NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
+
 
 function App() {
 
   return (
     <>
-      <div className="navbar bg-base-300 shadow-sm">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">🧑‍💻 DevTinder</a>
-      </div>
-      <div className="flex gap-2">
-
-        <div className="dropdown dropdown-end mx-5">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-            </div>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li><a>Settings</a></li>
-            <li><a>Logout</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-      <h1 className="text-3xl font-bold text-yellow-600">Hello world! chal gya tailwind</h1>
-      <p className="text-red-400">in red color</p>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body/>}>
+          { // children routes 
+            // children component can not be directly render here 
+            // children needs to render in parent component using <outlet/>
+          }
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/profile" element= {<Profile/>}/>
+          </Route>
+          
+        </Routes>
+      </BrowserRouter>
+      {/* <NavBar/> */}
+      {/* <h1 className="text-3xl font-bold text-yellow-600">Hello world! chal gya tailwind</h1>
+      <p className="text-red-400">in red color</p> */}
     </>
   )
 }
