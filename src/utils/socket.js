e44c5a2f81f361dc7b6b6e763f696eaea2b33164
Himanshu Kakkar <himanshukakkar917@gmail.com>
2025-10-29 23:23:40 +0530

    update socket constants

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,10 +1,19 @@
 import io from "socket.io-client";
 import { BASE_URL } from "./constants";
 
+
 export const createSocketConnection = () => {
-  if (location.hostname === "localhost") {
-    return io(BASE_URL);
-  } else {
-    return io("/", { path: "/socket.io" });
-  }
+  return io(BASE_URL, {
+    transports: ["websocket"],
+    path: "/socket.io",
+  });
 };
+
+
+// export const createSocketConnection = () => {
+//   if (location.hostname === "localhost") {
+//     return io(BASE_URL);
+//   } else {
+//     return io("/", { path: "/socket.io" });
+//   }
+// };
