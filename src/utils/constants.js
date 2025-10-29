9972358f1713307a5c3ed3948913dd5334576b06
Himanshu Kakkar <himanshukakkar917@gmail.com>
2025-10-29 23:13:13 +0530

    update constants urls

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,18 @@
 // export const BASE_URL = "http://localhost:7777"
 
 // Production URL
-export const BASE_URL = 
-    location.hostname === "localhost" ? "http://localhost:7777" : "/api";
+// export const BASE_URL = 
+//     location.hostname === "localhost" ? "http://localhost:7777" : "/api";
+
+
+// export const BASE_URL =
+//     (typeof import.meta !== "undefined" && import.meta.env && import.meta.env.VITE_API_BASE_URL)
+//         ? import.meta.env.VITE_API_BASE_URL
+//         : (location.hostname === "localhost" ? "http://localhost:7777" : "/api");
+
+export const BASE_URL =
+  location.hostname === "localhost"
+    ? "http://localhost:7777" // Localhost
+    : location.hostname.includes("onrender.com")
+    ? "https://devconnect-no9b.onrender.com" // Render
+    : "/api"; // AWS
